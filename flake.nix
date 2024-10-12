241d9d90e25740f934210859635c837377d05b12
Vladislav <ghoulyaev@gmail.com>
2024-10-12 16:07:08 +0300

    initial commit
    Add yarn pm to project with telegraf&typescript

--- flake.nix
+++ flake.nix
@@ -11,11 +11,6 @@
         in {
             devShells.default = pkgs.mkShell {
                 nativeBuildInputs = with pkgs; [ nodejs yarn nodePackages.ts-node ];
-                env = {
-                    PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-                    PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-                    PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
-                };
                 shellHook = ''
                     export PATH=$PATH:$PWD/node_modules/.bin
                 '';
